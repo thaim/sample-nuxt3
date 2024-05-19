01203677aa171fdf143dba7692074067c7dc2215
thaim <thaim24@gmail.com>
2024-05-19 17:53:53 +0900

    implement count pages

---
+++ storages/pages/count.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const counterStore = useCounterStore();
+const { increment } = counterStore;
+const { count } = storeToRefs(counterStore);
+</script>
+
+<template>
+    <div>
+        <h1>Count: {{ count }}</h1>
+        <button @click="increment">+1する</button>
+        <nuxt-link to="/">戻る</nuxt-link>
+    </div>
+</template>
---
+++ storages/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const counter = useCounterStore();
+</script>
+
+<template>
+    <div>
+        <nuxt-link to="/count">countページ</nuxt-link>
+        <p>ボタンを{{ counter.count }}回押しました</p>
+    </div>
+</template>
