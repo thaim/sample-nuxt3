d2aed308faef00c3e3d6e213f11fd8ce641667fd
thaim <thaim24@gmail.com>
2024-05-19 18:02:11 +0900

    merge pages into app.vue

--- storages/app.vue
+++ storages/app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const counterStore = useCounterStore();
+const { increment } = counterStore;
+const { count } = storeToRefs(counterStore);
+</script>
+
 <template>
   <div>
-    <NuxtPage />
+    <p>ボタンを{{ counterStore.count }}回押しました</p>
+    <button @click="increment">+1する</button>
   </div>
 </template>
